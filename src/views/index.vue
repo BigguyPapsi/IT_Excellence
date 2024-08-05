fe2d6f05e0ee204e8465b10824c44e646f953e08
ThanouPap <papartztv@gmail.com>
2024-08-05 16:02:44 +0700

    first commit

---
+++ src/views/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+<h1>Home page</h1>
+<v-btn
+  elevation="2"
+>Button</v-btn>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
